8a60767d388cdf549c96aa4631061b01ccc4df57
Michael McCarthy <hencethus@gmail.com>
2017-01-07 13:36:06 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_assets import Environment, Bundle
+
+app = Flask(__name__)
+assets = Environment(app)
+
+app.config['TYPESCRIPT_BIN'] = 'node_modules/typescript/bin/tsc'
+
+ts = Bundle('ts/app.ts', filters='typescript', output='gen/packed.js')
+assets.register('js_all', ts)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.config['ASSETS_DEBUG'] = True
+    app.run(debug=True)
