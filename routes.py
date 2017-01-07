02832d34f9c07c2eddc64da431f561f1f60a0d25
Michael McCarthy <hencethus@gmail.com>
2017-01-07 14:11:26 -0500

    remove flask_assets impelemntation

--- routes.py
+++ routes.py
@@ -4,10 +4,10 @@ from flask_assets import Environment, Bundle
 app = Flask(__name__)
 assets = Environment(app)
 
-app.config['TYPESCRIPT_BIN'] = 'node_modules/typescript/bin/tsc'
+#app.config['TYPESCRIPT_BIN'] = 'node_modules/typescript/bin/tsc'
 
-ts = Bundle('ts/app.ts', filters='typescript', output='gen/packed.js')
-assets.register('js_all', ts)
+#ts = Bundle('ts/app.ts', filters='typescript', output='gen/packed.js')
+#assets.register('js_all', ts)
 
 @app.route('/')
 def index():
