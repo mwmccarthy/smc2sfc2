4b73acef5260b713c9d311ecf5441ae91ae35bbf
Michael McCarthy <hencethus@gmail.com>
2017-01-07 21:12:22 -0500

    Edit config

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
+import os
+DEBUG = os.environ.get('DEBUG')
 TYPESCRIPT_BIN = 'node_modules/.bin/tsc'
--- routes.py
+++ routes.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template
 from flask_assets import Environment, Bundle
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
 
 assets = Environment(app)
 
@@ -13,6 +12,3 @@ assets.register('js_all', ts)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
